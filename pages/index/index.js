7bb4c2c6299b100cfad9ebd9f0cdc57019c3d5dd
dpeng1203 <d_p1203@163.com>
2018-06-08 18:27:19 +0800

    动态小时天气

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,37 +9,62 @@ const weatherMap = {
 
 Page({
   data: {
-
+    items: []
   },
+
   onPullDownRefresh () {
       this.getNow( () => {
         wx.stopPullDownRefresh()
       })
   },
+
   onLoad() {
     this.getNow()
   },
+
   getNow ( callback ) {
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
-      data: {
-        city: '广州市'
-      },
+      data: { city: '广州市'},
+
       success: res => {
-       // console.log(res)
+       //console.log(res)
         let result = res.data.result
-        let temp = result.now.temp + '°'
-        let weather = result.now.weather
-        console.log(temp, weather)
-        this.setData({
-          nowTemp: temp,
-          nowWeather: weatherMap[weather]
-        })
+        this.setNow(result)
+        this.setHour(result)
       },
+
       complete: () => {
         callback && callback()
       }
     })
-  }
+  },
 
+  setNow(result) {
+    let temp = result.now.temp + '°'
+    let weather = result.now.weather
+    console.log(temp, weather)
+    this.setData({
+      nowTemp: temp,
+      nowWeather: weatherMap[weather]
+    })
+  },
+
+  setHour (result) {
+    let items = []
+    let nowHour = new Date().getHours()
+    let forecast = result.forecast
+    console.log(forecast)
+    for (let i = 0; i < 24; i += 3) {
+      items.push({
+        time: (i + nowHour) % 24 + '时',
+        icon: '/images/' + forecast[i / 3].weather + '-icon.png',
+        temp: forecast[i / 3].temp + '°'
+      })
+    }
+    items[0].time = '现在'
+    this.setData({
+      items: items
+    })
+  }
 })
